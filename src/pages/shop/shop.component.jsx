86b1c335457bf961701db0f9cd1169030f4d4b08
khashayar <khashayar.sasanni@hotmail.com>
2019-08-20 13:27:31 +0200

    Can't resolve

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import SHOP_DATA from './shop.data.js';
+import collectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collection: SHOP_DATA
+        }
+    }
+
+    render() {
+        const { collections } = this.state;
+        return (<div className='shop-page'>
+            {
+                collections.map(({ id, ...otherCollectionProps }) => (
+                    <collectionPreview key={id} {...otherCollectionProps} />
+                ))
+            }
+        </div>);
+    }
+}
+
+export default ShopPage;
