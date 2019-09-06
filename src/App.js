7b8c42f1847aceac9af277c3e55a930d892ac114
khashayar <khashayar.sasanni@hotmail.com>
2019-09-06 13:57:15 +0200

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our withSpinner HOC

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -26,7 +26,7 @@ class App extends React.Component {
           setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            })
+            });
         });
       }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,42 +3,74 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyDVwpdNeWoREfPpzGUMnLBYPe2s4iwFq4Q",
-    authDomain: "crwn-db-ec859.firebaseapp.com",
-    databaseURL: "https://crwn-db-ec859.firebaseio.com",
-    projectId: "crwn-db-ec859",
-    storageBucket: "",
-    messagingSenderId: "815736462783",
-    appId: "1:815736462783:web:02c11af0cecf7b80"
-};
-
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-    const snapShot = await userRef.get();
-
-    if(!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createAt = new Date();
-
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createAt,
-                ...additionalData
-            })
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
-    }
-
-    return userRef;
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
